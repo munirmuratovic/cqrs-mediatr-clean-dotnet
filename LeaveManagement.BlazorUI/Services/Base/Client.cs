871789c0fdf41a8941f5d5a6ae39342a4033d76f
munirmuratovic <munirmuratovicx@gmail.com>
2024-12-16 21:26:34 +0100

    Adding Blazor UI and nswag contracts

---
+++ LeaveManagement.BlazorUI/Services/Base/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.BlazorUI.Services.Base;
+
+public partial class Client : IClient
+{
+    public HttpClient HttpClient
+    {
+        get { return _httpClient; }
+    }
+}
